60d3575f57709203dfee3e442366aaf955600cba
Nachipatel03 <nachipatel003@outlook.com>
2026-03-16 17:37:05 +0530

    added sendGrid

--- GymMate/GymMate/settings.py
+++ GymMate/GymMate/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     "diets",
     "payments",
     'django_apscheduler',
+    'anymail',
 ]
 
 MIDDLEWARE = [
@@ -175,10 +176,8 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 # Email Configuration
-EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.console.EmailBackend')
-EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+EMAIL_BACKEND = "anymail.backends.sendgrid.EmailBackend"
+ANYMAIL = {
+    "SENDGRID_API_KEY": os.getenv("SENDGRID_API_KEY"),
+}
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'GymMate <noreply@gymmate.com>')
